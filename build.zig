1a860e30398edf78b8de4cfb42866654573bf98d
CodeHz <codehz@outlook.com>
2020-09-02 08:59:30 +0000

    fix newline

--- build.zig
+++ build.zig
@@ -4,9 +4,7 @@ const Builder = std.build.Builder;
 pub fn build(b: *Builder) void {
     b.setPreferredReleaseMode(.ReleaseFast);
     const mode = b.standardReleaseOptions();
-    const target = std.zig.CrossTarget.parse(.{
-        .arch_os_abi = "wasm32-freestanding"
-    }) catch unreachable;
+    const target = std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-freestanding" }) catch unreachable;
     const lib = b.addStaticLibrary("engine", "src/main.zig");
     lib.setTarget(target);
     lib.setBuildMode(mode);
