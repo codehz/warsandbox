779fe3ef8f9a82b77221905f081c8c2cdf141020
CodeHz <codehz@outlook.com>
2020-09-10 05:14:50 +0000

    fix default shadow mode

--- web/src/index.ts
+++ web/src/index.ts
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
