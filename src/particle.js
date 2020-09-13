76505f3b07aa157ad93071d434c69569d8081f4b
codehz <codehz@users.noreply.github.com>
2020-09-13 13:44:07 +0000

    deploy: 7921f9808a28fa3b1ee63ceec395bb929c3afbec

--- src/particle.js
+++ src/particle.js
@@ -1,4 +1,4 @@
-import*as e from"../web_modules/three.js";const n=`
+import*as e from"../web_modules/three.js";const a=`
   attribute float size;
   attribute vec3 particleColor;
 
@@ -7,13 +7,13 @@ import*as e from"../web_modules/three.js";const n=`
   void main() {
     vColor = particleColor;
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = size * (300.0 / -mvPosition.z);
+    gl_PointSize = size * (1400.0 / -mvPosition.z);
     gl_Position = projectionMatrix * mvPosition;
   }
-`,a=`
+`,s=`
   varying vec3 vColor;
 
   void main() {
     gl_FragColor = vec4(vColor, 1.0);
   }
-`;export function createParticleSystem(o){const t=new e.BufferGeometry();t.boundingBox=new e.Box3(),t.boundingSphere=new e.Sphere(),t.setAttribute("size",new e.InterleavedBufferAttribute(o,1,0)),t.setAttribute("position",new e.InterleavedBufferAttribute(o,3,1)),t.setAttribute("color",new e.InterleavedBufferAttribute(o,3,4));const r=new e.ShaderMaterial({vertexShader:n,fragmentShader:a,transparent:!0}),i=new e.Points(t,r);return i}
+`;export function createParticleSystem(o){const r=o.proxy(new e.InterleavedBuffer(o.data,7)),t=new e.BufferGeometry();t.boundingSphere=new e.Sphere(new e.Vector3(32,512,128),1e3),t.setAttribute("size",new e.InterleavedBufferAttribute(r,1,0)),t.setAttribute("position",new e.InterleavedBufferAttribute(r,3,1)),t.setAttribute("particleColor",new e.InterleavedBufferAttribute(r,3,4));const i=new e.ShaderMaterial({vertexShader:a,fragmentShader:s,transparent:!0,depthWrite:!1}),n=new e.Points(t,i);return n}
